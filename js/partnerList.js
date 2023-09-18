0b23bdb7f99330dd3b250aaef20f20bac3e55fed
Ørjan Berger <79088620+oerjanberger@users.noreply.github.com>
2023-09-18 15:18:07 +0200

    Updated to work on deployed site

--- js/partnerList.js
+++ js/partnerList.js
@@ -1,5 +1,5 @@
 /**
- * @typedef {import("./repList").Representative} Representative
+ * @typedef {import("ScanReach.WebsiteMap/repList").Representative} Representative
  */
 
 /**
@@ -10,9 +10,9 @@ const salesPartners = [
     id: 1,
     name: "Bocvera Maritime",
     area: "Mexico",
-    img: "./images/salesPartners/bocveraLogo.png",
+    img: "ScanReach.WebsiteMap/images/salesPartners/bocveraLogo.png",
     lngLat: [-96.11194, 19.15939],
-    geoJson: "./borders/mexicoSimplified.geojson",
+    geoJson: "ScanReach.WebsiteMap/borders/mexicoSimplified.geojson",
     link: "https://bocvera.com/",
     description:
       "Bocvera Maritime S.A. de C.V. is a Mexican company that provides services in the maritime sector, with a focus on the commercialization of products and services for the maritime industry. They are a company that is committed to the environment and the development of the maritime sector in Mexico.",
@@ -21,9 +21,9 @@ const salesPartners = [
     id: 2,
     name: "Querin",
     area: "Germany",
-    img: "./images/salesPartners/querinLogo.png",
+    img: "ScanReach.WebsiteMap/images/salesPartners/querinLogo.png",
     lngLat: [9.54435, 53.53786],
-    geoJson: "./borders/germanySimplified.geojson",
+    geoJson: "ScanReach.WebsiteMap/borders/germanySimplified.geojson",
     link: "https://www.querin.de/",
     description:
       "Querin GmbH is a German company that provides services in the maritime sector, with a focus on the commercialization of products and services for the maritime industry. They are a company that is committed to the environment and the development of the maritime sector in Germany.",
@@ -32,9 +32,9 @@ const salesPartners = [
     id: 3,
     name: "DSolution",
     area: "Spain",
-    img: "./images/salesPartners/dSolutionLogo.png",
+    img: "ScanReach.WebsiteMap/images/salesPartners/dSolutionLogo.png",
     lngLat: [-3.70379, 40.41678], // Not the correct coordinates - The location is in panama. Which will only confuse the user
-    geoJson: "./borders/spainSimplified.geojson",
+    geoJson: "ScanReach.WebsiteMap/borders/spainSimplified.geojson",
     link: null, // Not the correct link
     description:
       "DSolution is a Spanish company that provides services in the maritime sector, with a focus on the commercialization of products and services for the maritime industry. They are a company that is committed to the environment and the development of the maritime sector in Spain.",
@@ -43,9 +43,9 @@ const salesPartners = [
     id: 4,
     name: "JNK",
     area: "Indonesia",
-    img: "./images/salesPartners/jnkLogo.png",
+    img: "ScanReach.WebsiteMap/images/salesPartners/jnkLogo.png",
     lngLat: [106.89373, -6.15456],
-    geoJson: "./borders/indonesiaSimplified.geojson",
+    geoJson: "ScanReach.WebsiteMap/borders/indonesiaSimplified.geojson",
     link: null, // Not the correct link
     description:
       "JNK is an Indonesian company that provides services in the maritime sector, with a focus on the commercialization of products and services for the maritime industry. They are a company that is committed to the environment and the development of the maritime sector in Indonesia.",
@@ -54,9 +54,9 @@ const salesPartners = [
     id: 5,
     name: "Lyssos",
     area: "Greece",
-    img: "./images/salesPartners/lyssosLogo.png",
+    img: "ScanReach.WebsiteMap/images/salesPartners/lyssosLogo.png",
     lngLat: [23.63549, 37.93688],
-    geoJson: "./borders/greeceSimplified.geojson",
+    geoJson: "ScanReach.WebsiteMap/borders/greeceSimplified.geojson",
     link: "https://www.lyssos.com/",
     description:
       "Lyssos is a Greek company that provides services in the maritime sector, with a focus on the commercialization of products and services for the maritime industry. They are a company that is committed to the environment and the development of the maritime sector in Greece.",
@@ -65,9 +65,9 @@ const salesPartners = [
     id: 6,
     name: "Seacoast Marine Electronics",
     area: "Canada",
-    img: "./images/salesPartners/seacoastMarineLogo.png",
+    img: "ScanReach.WebsiteMap/images/salesPartners/seacoastMarineLogo.png",
     lngLat: [-63.66494, 44.63718],
-    geoJson: "./borders/canadaSimplified.geojson",
+    geoJson: "ScanReach.WebsiteMap/borders/canadaSimplified.geojson",
     link: "https://www.seacoastmarine.ca/",
     description:
       "Seacoast Marine Electronics is a Canadian company that provides services in the maritime sector, with a focus on the commercialization of products and services for the maritime industry. They are a company that is committed to the environment and the development of the maritime sector in Canada.",
@@ -76,9 +76,9 @@ const salesPartners = [
     id: 7,
     name: "Technonaval",
     area: "USA",
-    img: "./images/salesPartners/technonavalLogo.png",
+    img: "ScanReach.WebsiteMap/images/salesPartners/technonavalLogo.png",
     lngLat: [-80.40315, 25.65554],
-    geoJson: "./borders/unitedStatesSimplified.geojson",
+    geoJson: "ScanReach.WebsiteMap/borders/unitedStatesSimplified.geojson",
     link: "https://www.technonaval.com/",
     description:
       "Technonaval is a USA company that provides services in the maritime sector, with a focus on the commercialization of products and services for the maritime industry. They are a company that is committed to the environment and the development of the maritime sector in USA.",
@@ -87,9 +87,9 @@ const salesPartners = [
     id: 8,
     name: "Vision Marine Technologies",
     area: "Brazil",
-    img: "./images/salesPartners/visionMarineLogo.png",
+    img: "ScanReach.WebsiteMap/images/salesPartners/visionMarineLogo.png",
     lngLat: [-43.2673, -22.87791],
-    geoJson: "./borders/brazilSimplified.geojson",
+    geoJson: "ScanReach.WebsiteMap/borders/brazilSimplified.geojson",
     link: "https://www.visionmarine.com.br//",
     description:
       "Vision Marine Technologies is a Brazilian company that provides services in the maritime sector, with a focus on the commercialization of products and services for the maritime industry. They are a company that is committed to the environment and the development of the maritime sector in Brazil.",
